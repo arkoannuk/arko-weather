ea6cc67f2773b06f2fbacd0441f388f4f610ceee
arkoa <arkoannuk+prog@gmail.com>
2023-09-18 14:39:14 +0300

    Change: vite build config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-const path = require('path')
+const path = require("path")
 
 export default {
-  root: path.resolve(__dirname, 'src'),
+  root: path.resolve(__dirname, "src"),
+  build: {
+    outDir: "../dist",
+  },
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
     },
   },
   server: {
